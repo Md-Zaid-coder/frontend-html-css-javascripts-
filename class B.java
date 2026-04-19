0e1fe279adf1f726234d7115c95333a0ae8649ba
MD ZAID <mdzaid82404@gmail.com>
2026-04-19 23:56:00 +0530

    Add files via upload

---
+++ class B.java
@@ -0,0 +1,20 @@
+class B
+{
+    int age; String name;
+    B()
+    {
+        age=0; name=null;
+    }
+    void show()
+    {
+        System.out.println(age+"   "+name);
+    }
+}
+class A
+{
+    public static void main(String args[])
+    {
+        B sc= new A();
+        Sc.show();
+    }
+}
